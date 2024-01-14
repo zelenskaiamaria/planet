8ec55ead222c11de36d57a26c228938e70c9c9c6
zelenskaiamaria <121615313+zelenskaiamaria@users.noreply.github.com>
2024-01-14 16:28:50 +0300

    Create planetary.js

---
+++ planetary.js
@@ -0,0 +1,8 @@
+const strelk = document.querySelector('.strelka');
+const look = document.querySelector('.text__look');
+console.log(look);
+strelk.addEventListener('click',
+function(){
+    const look = document.querySelector('.text__look');
+    look.scrollIntoView(true);
+})
